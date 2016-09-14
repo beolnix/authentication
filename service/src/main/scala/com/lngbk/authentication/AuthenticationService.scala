5abd35f92c41c45983abd15932714a9117d710e3
beolnix <beolnix@gmail.com>
2016-09-14 09:48:28 +0200

    pep8 - cause I fell like doing a barrel roll

--- service/src/main/scala/com/lngbk/authentication/AuthenticationService.scala
+++ service/src/main/scala/com/lngbk/authentication/AuthenticationService.scala
@@ -1,21 +1,16 @@
 package com.lngbk.authentication
 
 
-import akka.actor.Props
 import com.lngbk.api.{AuthenticationApi, AuthenticationConstants}
 import com.lngbk.authentication.actor.AuthenticationActor
-import com.lngbk.commons.management.SystemManager
-import com.lngbk.commons.management.bootstrap.ServiceBootstrapDirector
-import org.slf4j.{Logger, LoggerFactory}
+import com.lngbk.commons.service.LngbkService
 
-/**
-  * Created by beolnix on 10/09/16.
-  */
-object AuthenticationService extends App {
-  private val logger: Logger = LoggerFactory.getLogger("AuthenticationService")
 
-  val api = new AuthenticationApi()
-  ServiceBootstrapDirector.initService(false, true)
-  SystemManager.system.actorOf(Props[AuthenticationActor], AuthenticationConstants.serviceName)
-  logger.info("Service started successfully")
+object AuthenticationService
+  extends LngbkService[AuthenticationActor](false, true, AuthenticationConstants.serviceName)
+  with App {
+
+  override def withDependencies(): Unit = {
+    AuthenticationApi()
+  }
 }
